6f2a42546a30a3a0c1679f5fcfc9dc02ce7752ce
hansol <waw8805@hanmail.net>
2024-05-17 13:07:56 +0900

    첫 화면 렌더링

---
+++ tumbler-shop/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // ensure you have an App.css or adjust the path accordingly
+import signInImage from './logo.png'; // "Sign in to tumbler" 이미지
+import kakaoLoginImage from './kakaoLogin.png'; // "카카오 로그인" 이미지
+
+function App() {
+  return (
+    <div className="App">
+      <img src={signInImage} alt="Sign in to tumbler" className="logo" />
+      <img src={kakaoLoginImage} alt="카카오 로그인" className="kakao-login" />
+    </div>
+  );
+}
+
+export default App;
